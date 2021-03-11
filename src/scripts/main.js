d501e531701b1fc6e38f789e710f70e1bd544da1
luzm321 <luzm321@yahoo.com>
2021-03-10 19:13:14 -0600

    Edited json files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,3 @@
-fetch('http://localhost:8088/pokemon').then(response =>  response.json()).then(data => {
-    console.log('data', data);
-})
+fetch("http://localhost:8088/pokemon").then(response =>  response.json()).then(pokemonData => {
+    console.log("data", pokemonData);
+});
